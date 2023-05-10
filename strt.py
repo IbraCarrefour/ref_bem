3a71d71c8097ea2b1aed30e0d9698cb97e4b2efa
COULIBALY BOURAHIMA <bourahima_coulibaly_1@ext.carrefour.com>
2023-05-10 08:48:28 +0200

    app

---
+++ strt.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+"## Classification basée sur les données apriori"
+
+def barcode():
+    barcod = st.text_input("Barcode" , value = 1, key = "barcode")
+    return barcod
+
+sparse_matrix = pd.read_csv('data.csv', sep = ',')
+sparse_matrix.astype({'barcode': str})
+#st.dataframe(sparse_matrix)
+def predict(barcode):
+  return sparse_matrix.loc[sparse_matrix.barcode == barcode]
+
+barcod = barcode()
+if barcod :
+   "codif possible"
+   st.dataframe(predict(barcod)[["barcode", "classe", "Probabilité"]].sort_values(by=['Probabilité']))
